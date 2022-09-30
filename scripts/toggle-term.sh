ce66707a50514155d16b3f78f95fea86395fa44e
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2022-09-30 08:19:19 -0700

    better terimnal toggle
    The window's size + position are now remembered when (un)toggling

--- scripts/toggle-term.sh
+++ scripts/toggle-term.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-#tdrop -h 98% kitty #-o background=#002220
-tdrop -h "" -w "" kitty #-o background=#002220
-#tdrop -c 'wmctrl -i -r $1 -b maximized_vert,maximized_horz' kitty
+# toggles a kitty terminal instance
+# bind this to a hotkey
+TERMINAL_CMD=kitty
+tdrop --no-manage --remember $TERMINAL_CMD
