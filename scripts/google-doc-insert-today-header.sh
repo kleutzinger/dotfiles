3723c4ad69a72a9cc97640414b25db24939651fa
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2022-12-12 08:50:13 -0800

    today header as qwerty

--- scripts/google-doc-insert-today-header.sh
+++ scripts/google-doc-insert-today-header.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 # while in google docs, insert today's date as a header
+# example:
+
+# Dec 12, 2022 (Mon)<newline>
 
 WEEKDAY=$(date +"%a")
 
@@ -9,10 +12,14 @@ xdotool getactivewindow key 'ctrl+alt+1'
 sleep 0.3
 # mousing over the `@` popup disrupts the selection
 xdotool mousemove 0 0
+# xdotool needs qwerty idk why
+setxkbmap us -option ctrl:swap_lalt_lctl
 xdotool type '@today'
 sleep 1
 xdotool key Return
 sleep 0.3
 xdotool type " ($WEEKDAY)"
 sleep 0.3
+# back to colemak
+setxkbmap us -variant colemak -option ctrl:swap_lalt_lctl
 xdotool key Return
