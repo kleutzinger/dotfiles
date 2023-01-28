c00ed798a99bc4d79d8622abff1505d59c92b7a8
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-01-27 21:59:22 -0800

    add white border around image for point selection

--- scripts/get_img_coords.py
+++ scripts/get_img_coords.py
@@ -12,7 +12,12 @@ import cv2 as cv
 import numpy as np
 from typing import List, Tuple
 
+# a list of (x,y) coordinates
 PointList = List[Tuple[int, int]]
+
+# add a border of this size around the image for point selection of near or
+# beyond the edge of the original image
+BORDER_SIZE_PX = 300
 
 # print(dir(cv2))
 
@@ -20,11 +25,22 @@ PointList = List[Tuple[int, int]]
 def choose_points(path: str) -> PointList:
     """
     make a popup to allow user input of corners of crt. use right click to set 4 points
+    points may exist ouside the original image
     """
     filename = path
     print(path)
     img = cv.imread(filename)
-    cv.imshow("window", img)
+    white = [255, 255, 255]
+    with_border = cv.copyMakeBorder(
+        img,
+        BORDER_SIZE_PX,
+        BORDER_SIZE_PX,
+        BORDER_SIZE_PX,
+        BORDER_SIZE_PX,
+        cv.BORDER_CONSTANT,
+        value=white,
+    )
+    cv.imshow("window", with_border)
     pts: list[tuple[int, int]]
     pts = []
 
@@ -33,11 +49,11 @@ def choose_points(path: str) -> PointList:
         d = -1 if d == 0 else d
         i = cv.getTrackbarPos("color", "window")
         color = (0, 0, 255)  # red
-        img[:] = img
-        cv.polylines(img, np.array([pts]), True, color, d)
-        cv.imshow("window", img)
-        text = f"color={color}, thickness={d}"
-        cv.displayOverlay("window", text)
+        with_border[:] = with_border
+        cv.polylines(with_border, np.array([pts]), True, color, d)
+        cv.imshow("window", with_border)
+        # text = f"color={color}, thickness={d}"
+        # cv.displayOverlay("window", text)
 
     def mouse(event, x, y, flags, param):
         if event == cv.EVENT_RBUTTONDOWN:
@@ -53,7 +69,14 @@ def choose_points(path: str) -> PointList:
         if k == 27:  # Esc key to stop
             break
     cv.destroyAllWindows()
-    return order_pts(pts)
+
+    def fix_border_coords(pts: PointList) -> PointList:
+        out = []
+        for x, y in pts:
+            out.append((x - BORDER_SIZE_PX, y - BORDER_SIZE_PX))
+        return out
+
+    return order_pts(fix_border_coords(pts))
 
 
 def order_pts(pts: PointList) -> PointList:
@@ -79,10 +102,10 @@ def order_pts(pts: PointList) -> PointList:
     top2 = set(sorted(pts, key=lambda p: p[1])[:2])
     bottom2 = set(sorted(pts, key=lambda p: p[1])[2:])
 
-    topleft = list(top2.intersection(left2))[0]
-    topright = list(top2.intersection(right2))[0]
-    bottomleft = list(bottom2.intersection(left2))[0]
-    bottomright = list(bottom2.intersection(right2))[0]
+    topleft = top2.intersection(left2).pop()
+    topright = top2.intersection(right2).pop()
+    bottomleft = bottom2.intersection(left2).pop()
+    bottomright = bottom2.intersection(right2).pop()
 
     return [topleft, topright, bottomleft, bottomright]
 
