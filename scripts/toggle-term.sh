923b9d17ae41a739dfc06690f5f25e29658c9de0
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-05 15:02:49 -0700

    better f1 / super + return terminal toggle
    i've moved away from using tdrop
    
    now i should basically only have a single instance of kitty at a time

--- scripts/toggle-term.sh
+++ scripts/toggle-term.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 # toggles a kitty terminal instance
 # bind this to a hotkey
-TERMINAL_CMD=kitty
-tdrop --no-manage --remember $TERMINAL_CMD
+# accepts the --new-window flag to open a new window inside the terminal
+
+# todo: add --new-tab to open a new tab altogether
+
+TERMINAL_CMD='kitty -o allow-remote-control=true --listen-on unix:/tmp/kitty'
+if [ "$1" = "--new-window" ]; then
+	NEW_WINDOW="true"
+else
+	NEW_WINDOW="false"
+fi
+
+kitty_ls="$(kitty @ --to unix:/tmp/kitty ls)"
+# if last command exit 1, then no kitty is running
+if [ $? -eq 1 ]; then
+	$TERMINAL_CMD
+	exit 0
+fi
+
+window_id=$(echo $kitty_ls | jq -r '.[].platform_window_id')
+is_focused=$(echo $kitty_ls | jq -r '.[].is_focused')
+if [ "$is_focused" = "true" ] && [ "$NEW_WINDOW" = "false" ]; then
+	xdotool windowminimize $window_id
+else
+	# focus
+	xdotool windowactivate $window_id
+	if [ "$NEW_WINDOW" = "true" ]; then
+		kitty @ --to unix:/tmp/kitty launch --type=window
+	fi
+fi
