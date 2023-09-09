7d74fcbdd2dc19763ed1d2568c7decea7fc7790f
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-08 20:18:49 -0700

    no restart no encode env2dokku.fish

--- scripts/env2dokku.fish
+++ scripts/env2dokku.fish
@@ -1,11 +1,11 @@
 #!/bin/fish
 
-# this script will read a .env file and output an encoded dokku config:set
+# this script will read a .env file and output a dokku config:set
 # command
 
 set env_file ".env"
 set app_name_probably (git rev-parse --show-toplevel | xargs basename)
-set output "dokku config:set --encoded $app_name_probably "
+set output "dokku config:set --no-restart $app_name_probably "
 
 
 # loop through all relevant lines in the .env file
@@ -13,9 +13,8 @@ for line in (cat $env_file | grep -v '^#' | grep -v '^$')
     # encode all values (not keys) with base64
     set -l line2 (string split -m 1 = $line)
     set -l key $line2[1]
-    set -l value $line2[2]
-    set -l value_enc (echo $value | base64)
-    set output "$output'$key=$value_enc' "
+    set -l value (echo $line2[2] | string trim --chars '\n')
+    set output "$output'$key=$value' "
 end
 
 echo $output
