e1b6cd6ef09d8087de3c2bc79432e65910efbfee
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-20 02:18:36 -0700

    include dokku version in backup

--- scripts/dokku-backup.sh
+++ scripts/dokku-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export BACKUP_TIME=$(date +%Y-%m-%d-%H-%M)
+export DOKKU_VERSION=$(dokku version | awk '{print $3}')
 sudo mkdir -p /var/lib/dokku/services
 sudo chown dokku:dokku /var/lib/dokku/services
 mkdir -p /tmp/dokku-backups/
-sudo tar -czvf "/tmp/dokku-backups/${BACKUP_TIME}-dokku-backup.tar.gz" /home/dokku /var/lib/dokku/config /var/lib/dokku/data /var/lib/dokku/services /var/lib/dokku/plugins
+sudo tar -czvf "/tmp/dokku-backups/${BACKUP_TIME}-dokku-${DOKKU_VERSION}-backup.tar.gz" /home/dokku /var/lib/dokku/config /var/lib/dokku/data /var/lib/dokku/services /var/lib/dokku/plugins
 
