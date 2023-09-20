70797fc45f434b46864df4f4d7f26f1ebf943a06
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-09-20 02:05:52 -0700

    introduce dokku-backup.sh

---
+++ scripts/dokku-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export BACKUP_TIME=$(date +%Y-%m-%d-%H-%M)
+sudo mkdir -p /var/lib/dokku/services
+sudo chown dokku:dokku /var/lib/dokku/services
+mkdir -p /tmp/dokku-backups/
+sudo tar -czvf "/tmp/dokku-backups/${BACKUP_TIME}-dokku-backup.tar.gz" /home/dokku /var/lib/dokku/config /var/lib/dokku/data /var/lib/dokku/services /var/lib/dokku/plugins
+
