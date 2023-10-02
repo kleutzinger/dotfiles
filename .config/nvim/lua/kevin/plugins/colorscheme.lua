19fffe1a4fb7a17531e70f0a09fc5fb816ef219d
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-10-02 02:41:20 -0700

    new colorscheme :D
    nice kitty theme

--- .config/nvim/lua/kevin/plugins/colorscheme.lua
+++ .config/nvim/lua/kevin/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
   {
-    "bluz71/vim-nightfly-guicolors",
+    "catppuccin/nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
+      vim.cmd([[colorscheme catppuccin]])
     end,
   },
 }
--- .config/nvim/lua/kevin/plugins/telescope.lua
+++ .config/nvim/lua/kevin/plugins/telescope.lua
@@ -28,6 +28,7 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
+    keymap.set("n", "<leader><leader>", "<cmd>Telescope<cr>", { desc = "Open Telescope" })
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
