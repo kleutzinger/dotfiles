7a17c59ef370ff8303a56a363f5369f04dcb5c3a
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-12-10 02:30:38 -0800

    have bandcamp downloader save to turso db

--- scripts/bandcamp-downloader.sh
+++ scripts/bandcamp-downloader.sh
@@ -7,14 +7,22 @@ if [ -z "$*" ]; then
    echo "No args, using clipboard: $DLS";
 fi
 
+# if user is named kevin, do the following
+if [ "$USER" = "kevin" ]; then
+  set -e
+  turso db shell bandcamps "insert into downloads (url) values('$DLS');"
+  set +e
+fi
+
 # put download here to start
 TMP=$(mktemp --directory)
 pushd $TMP
 yt-dlp -f bestaudio[ext=mp3] --embed-thumbnail -o  "%(artist)s - %(playlist_title)s/%(playlist_index)s %(track)s.%(ext)s" "$DLS"
-# folder with most subdirs / files contained therein
-# aka the most common artist of the album
-LARGEST_FOLDER="$(find . -type d | cut -d/ -f 2 | uniq -c | sort -g | tail -n 1 | awk '{$1=""}1' | awk '{$1=$1}1')"
+# folder with largest size
+# aka the most common artist of the album which we set as the album artist
+LARGEST_FOLDER="$(du --max-depth=1 . | sort -r -k1,1n | tail -2 | head -1 | cut -f2 | sed 's/^.\///')"
 popd
 mkdir "$LARGEST_FOLDER"
 find $TMP/ -name '*.mp3' -print0 | xargs -0 -I files cp files "$LARGEST_FOLDER"
 echo "$DLS" >> $HOME/Music/bandcamps.txt
+
