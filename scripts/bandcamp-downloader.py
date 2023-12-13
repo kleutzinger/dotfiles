173eae0fd28f759120b647f38b96db9e28f393a4
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-12-12 21:17:30 -0800

    move to bandcamp-downloader.py

---
+++ scripts/bandcamp-downloader.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import tempfile
+import sys
+import shutil
+from urllib.parse import urlsplit, urlunsplit
+
+
+def remove_query_params_and_fragment(url):
+    return urlunsplit(urlsplit(url)._replace(query="", fragment=""))
+
+
+def main():
+    DL_URL = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if not DL_URL:
+        DL_URL = subprocess.check_output(["xsel", "-ob"]).decode().strip()
+        print(f"No args, using clipboard: {DL_URL}")
+
+    if "?" in DL_URL or "#" in DL_URL:
+        DL_URL = remove_query_params_and_fragment(DL_URL)
+        print(f"Removed query params: {DL_URL}")
+
+    # if user is named kevin, do the following
+    if os.environ.get("USER") == "kevin":
+        subprocess.run(
+            [
+                "turso",
+                "db",
+                "shell",
+                "bandcamps",
+                f"insert into downloads (url) values('{DL_URL}');",
+            ]
+        )
+
+    # put download here to start
+    tmp_dir = tempfile.mkdtemp(prefix="bandcamp-")
+    start_dir = os.getcwd()
+    os.chdir(tmp_dir)
+
+    subprocess.run(
+        [
+            "yt-dlp",
+            "-f",
+            "bestaudio[ext=mp3]",
+            "--embed-thumbnail",
+            "-o",
+            "%(artist)s - %(playlist_title)s/%(playlist_index)s %(track)s.%(ext)s",
+            DL_URL,
+        ]
+    )
+
+    # folder with largest size
+    # aka the most common artist of the album which we set as the album artist
+    largest_folder = max(os.listdir(), key=lambda x: os.path.getsize(x))
+    os.chdir(start_dir)
+    # capitalize first letter of largest_folder
+    output_dir = os.path.join(start_dir, largest_folder[0].upper() + largest_folder[1:])
+    os.mkdir(output_dir)
+
+    for root, _, files in os.walk(tmp_dir):
+        for file in files:
+            if file.endswith(".mp3"):
+                source_path = os.path.join(root, file)
+                shutil.copy2(source_path, output_dir)
+
+    with open(os.path.join(os.environ["HOME"], "Music", "bandcamps.txt"), "a") as f:
+        f.write(f"{DL_URL}\n")
+
+
+if __name__ == "__main__":
+    main()
