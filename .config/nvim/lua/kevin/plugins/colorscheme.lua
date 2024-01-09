6c3ad015d15c0af4d95aea11fba0506a7323a569
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-01-09 02:04:50 -0800

    single nvim colorscheme

--- .config/nvim/lua/kevin/plugins/colorscheme.lua
+++ .config/nvim/lua/kevin/plugins/colorscheme.lua
@@ -1,22 +1,9 @@
-return os.getenv("TERM") ~= "xterm-kitty"
-    and {
-      {
-        -- not xterm, meaning it's likely a GUI (aka my notes)
-        "sainnhe/everforest",
-        priority = 1000,
-        config = function()
-          vim.cmd([[colorscheme everforest]])
-        end,
-      },
-    }
-  or {
-    {
-      -- normal xterm stuff
-      "catppuccin/nvim",
-      priority = 1000, -- make sure to load this before all the other start plugins
-      config = function()
-        -- load the colorscheme here
-        vim.cmd([[colorscheme catppuccin]])
-      end,
-    },
-  }
+return {
+  -- normal xterm stuff
+  "catppuccin/nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    -- load the colorscheme here
+    vim.cmd([[colorscheme catppuccin]])
+  end,
+}
