359a9d6cf152bed7c5aee0d5d53c5655342f6a61
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-01-31 00:04:53 -0800

    pkgfile on bootstrap

--- .config/yadm/bootstrap.fish
+++ .config/yadm/bootstrap.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
-yay -S --needed (cat ./arch_packages.txt)
-
+yay -S --needed (cat ./arch_packages.txt) --noconfirm
+sudo pkgfile --update
+systemctl enable --now pkgfile-update.timer
