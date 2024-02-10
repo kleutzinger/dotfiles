c67173cd0ea3ae7d5975d908bc57adf2181f3635
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-02-10 04:25:07 -0800

    hrefy is a bit better now

--- scripts/hrefy.fish
+++ scripts/hrefy.fish
@@ -3,31 +3,32 @@
 # Reads clipboard for a link and prompts for TEXT
 # generates <a href=""$LINK"> $TEXT </a>
 
+
 set LINK (xclip -o)
 set regex '(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 
 if string match -r $regex $LINK
     echo "Found link in clipboard"
 else
-    # prompt user for link
-    read -P "URL: " LINK
+    echo "No link found in clipboard"
+    exit 1
 end
 
-echo $LINK
+# get website.com from arbitrary urls / links
+set SITE_NAME (echo $LINK | perl -l -0777 -ne 'print $1 if /https?:\/\/(www\.)?([a-zA-Z0-9-]+)\.[a-zA-Z0-9-]+/')
 
-if not set -q argv[1]
-    # Prompt for <a>TEXT</a>
-    # get <title> tag from page, hopefully
-    set TEXT (wget -qO- $LINK | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
-else
-    # TEXT Found in args
-    set TEXT $argv[1]
+set TEXT (wget -qO- $LINK | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+# if no title found, use site name
+if test -z $TEXT
+    set TEXT $SITE_NAME
 end
+
+# make sure text is unescaped
+set TEXT (echo $TEXT | perl -MHTML::Entities -pe 'decode_entities($_);')
 
 set HREFD "<a href=\"$LINK\">$TEXT</a>"
-set HREFDMD "[$TEXT]($LINK)"
-echo -e "\t$HREFD"
+# set HREFDMD "[$TEXT]($LINK)"
+# echo -e "\t$HREFD"
 echo -e "\t$HREFDMD"
-echo "Put in clipboard?"
-read -P "Ctrl+C to cancel: " YESNO
 echo $HREFD | xclip -selection clipboard -in
+notify-send "Copied to clipboard: $HREFD"
