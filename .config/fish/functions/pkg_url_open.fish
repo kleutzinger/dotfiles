581ed94c45f2fbab4bc4911a2ad59cf06bae3e11
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-04-08 11:04:44 -0400

    pkg_url_open fzf

--- .config/fish/functions/pkg_url_open.fish
+++ .config/fish/functions/pkg_url_open.fish
@@ -1,4 +1,7 @@
 function pkg_url_open --description "open package's source code or homepage"
+    if test -z $argv
+        set argv (yay -Q | awk '{print $1}' | fzf)
+    end
     set url (pacman -Qi $argv | grep URL | grep -oE '[^ ]+$')
     echo $url
     python -m webbrowser -t "$url"
