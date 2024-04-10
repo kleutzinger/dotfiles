63d47904e278e876ab647064250f9c9858a48bda
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-04-10 03:19:25 -0400

    smaller volume steps

--- .config/fish/functions/pkg_url_open.fish
+++ .config/fish/functions/pkg_url_open.fish
@@ -1,6 +1,6 @@
 function pkg_url_open --description "open package's source code or homepage"
     set pkg (yay -Slaq |  fzf)
-    set url (yay -Gp $pkg | grep '^url=' | grep -oP '(?<=\")[^\"]+(?=\")')
+    set url (yay -Gp $pkg | grep '^url=' | grep -oP "(?<=\")[^\"]+(?=\")|(?<=').*(?=')")
     echo $url
     python -m webbrowser -t "$url"
 end
