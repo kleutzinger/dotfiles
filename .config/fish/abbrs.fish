e9b04da5ebd7ff9579d44ebad2a55eeb878506be
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-05-22 08:52:21 -0400

    trim some extra abbrs

--- .config/fish/abbrs.fish
+++ .config/fish/abbrs.fish
@@ -2,17 +2,14 @@ abbr q exit
 abbr lg lazygit
 abbr advent "source /home/kevin/gits/advent-of-code-2021/new_day.fish"
 abbr remove_orphans "sudo pacman -Qtdq | sudo pacman -Rns -"
-# abbr v "source .venv/bin/activate.fish"
 abbr pdb "python -m pdb"
 abbr ytp "mpv (xclip -o)"
 abbr :q exit
 abbr gll 'git log --graph --pretty=oneline --abbrev-commit'
 abbr hm 'history --merge'
 abbr yg 'lazygit -w ~ -g ~/.local/share/yadm/repo.git'
-abbr arstarst 'setxkbmap us -option ctrl:swap_lalt_lctl'
-abbr asdfasdf 'setxkbmap us -variant colemak -option ctrl:swap_lalt_lctl'
-abbr pp pm2
-abbr ppd 'pm2 delete all'
+abbr arst 'setxkbmap us -option ctrl:swap_lalt_lctl'
+abbr asdf 'setxkbmap us -variant colemak -option ctrl:swap_lalt_lctl'
 abbr d deactivate
 abbr gs 'git switch -'
 abbr xt 'TERM=xterm'
