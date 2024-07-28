9ecb713195c8110a470a22e237843769289e870c
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-07-28 01:50:12 -0400

    try using copilot.lua instead of copilot.vim

--- .config/nvim/lua/kevin/plugins/init.lua
+++ .config/nvim/lua/kevin/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
-  "github/copilot.vim",
   "nvim-treesitter/playground",
   "sbdchd/neoformat",
   "weilbith/nvim-code-action-menu",
