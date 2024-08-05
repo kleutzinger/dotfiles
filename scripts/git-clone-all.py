f3b94ed17bf23aee59c49f8358cd921b551b441b
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-08-04 22:30:18 -0400

    add orgname to git-clone-all.py

--- scripts/git-clone-all.py
+++ scripts/git-clone-all.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 """
-clone all repos from my own github
+clone all repos from my a given github org
 """
 
+import subprocess
+import json
+import sys
+
+
 def main():
-    # uset the gh command
-    import subprocess
-    import json
+    # get orgname from argv
+    if len(sys.argv) < 2:
+        print("Usage: git-clone-all.py orgname")
+        sys.exit(1)
+    orgname = sys.argv[1]
+    if orgname == "me":
+        orgname = ""
 
     repo_names = subprocess.check_output(
-        ["gh", "repo", "list", "--json", "name"]
+        ["gh", "repo", "list", orgname, "--json", "name", "--limit", "200"]
     ).decode("utf-8")
     for repo in json.loads(repo_names):
         repo_name = repo["name"]
         print(f"cloning {repo_name}")
-        subprocess.run(["gh", "repo", "clone", repo_name])
+        subprocess.run(["gh", "repo", "clone", f"{orgname}/{repo_name}"])
+
 
 main()
