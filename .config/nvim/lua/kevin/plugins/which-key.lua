bf74a799c530adfeec925b6a27c66c91a0b8cadb
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-08-05 01:26:13 -0400

    fix which-key

--- .config/nvim/lua/kevin/plugins/which-key.lua
+++ .config/nvim/lua/kevin/plugins/which-key.lua
@@ -5,13 +5,5 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    triggers_blacklist = {
-      -- ignore h when in insert mode (i use hk to exit insert mode)
-      i = { "h", "k" },
-    },
-  },
+  opts = {},
 }
