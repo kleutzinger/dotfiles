8cbf61cf18ce8245b0c9db6619b360ea0f7866ef
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-09-02 20:54:00 -0400

    add auto power profile switching
    from https://archive.is/Vh4ux

---
+++ .local/bin/power_monitor.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# from https://kobusvs.co.za/blog/power-profile-switching/
+# or   https://archive.is/wip/Vh4ux
+
+BAT=$(echo /sys/class/power_supply/BAT*)
+BAT_STATUS="$BAT/status"
+BAT_CAP="$BAT/capacity"
+LOW_BAT_PERCENT=0
+
+AC_PROFILE="performance"
+BAT_PROFILE="power-saver"
+LOW_BAT_PROFILE="power-saver"
+
+# wait a while if needed
+[[ -z $STARTUP_WAIT ]] || sleep "$STARTUP_WAIT"
+
+# start the monitor loop
+prev=0
+
+while true; do
+	# read the current state
+	if [[ $(cat "$BAT_STATUS") == "Discharging" ]]; then
+		if [[ $(cat "$BAT_CAP") -gt $LOW_BAT_PERCENT ]]; then
+			profile=$BAT_PROFILE
+		else
+			profile=$LOW_BAT_PROFILE
+		fi
+	else
+		profile=$AC_PROFILE
+	fi
+
+	# set the new profile
+	if [[ $prev != "$profile" ]]; then
+		echo setting power profile to $profile
+		powerprofilesctl set $profile
+    notify-send --expire-time=2000 "Power profile set to $profile"
+	fi
+
+	prev=$profile
+
+	# wait for the next power change event
+	inotifywait -qq "$BAT_STATUS" "$BAT_CAP"
+done
