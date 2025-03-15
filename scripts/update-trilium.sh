8117195e0abd05780b0620a07be2cd998d6ba8df
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-03-15 11:54:04 -0400

    update-trilium - fzf tag choosing

--- scripts/update-trilium.sh
+++ scripts/update-trilium.sh
@@ -5,18 +5,18 @@
 set -e
 set -x
 
-TAG="v0.92.3-beta"
+TAG=$(gh release list --repo TriliumNext/Notes --json tagName,publishedAt --jq '.[] | .tagName + "      " + .publishedAt' | fzf | awk '{print $1}')
 
 # this is the dir in the home directory where the trilium-next will be installed
-DIRNAME=trilium-next
+INSTALL_DIR=trilium-next
 DL_URL="https://github.com/TriliumNext/Notes/releases/download/$TAG/TriliumNextNotes-$TAG-linux-x64.zip"
 
 cd $(mktemp --directory --suffix=trilium-update)
 wget --verbose $DL_URL
 unzip *.zip
 rm *.zip
-rm -rf ~/$DIRNAME
+rm -rf ~/$INSTALL_DIR
 mv * ~/trilium-next
 # ensure symlink to ~/.local/bin/trilium
 rm -f ~/.local/bin/trilium
-ln -sf ~/$DIRNAME/trilium ~/.local/bin/trilium
+ln -sf ~/$INSTALL_DIR/trilium ~/.local/bin/trilium
