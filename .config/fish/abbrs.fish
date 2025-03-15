342166aba796a3c6fc4e0078b7886932f863abd8
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-03-15 02:32:35 -0400

    r for reset

--- .config/fish/abbrs.fish
+++ .config/fish/abbrs.fish
@@ -31,6 +31,7 @@ abbr ccc 'coconut.js --sec'
 abbr ccl 'coconut.js --list'
 abbr yayy 'yay --noconfirm'
 abbr cdt 'cd ~/.threads && gallery-dl (p) --sleep 5'
+abbr r 'reset'
 abbr -a L --position anywhere --set-cursor "% | less"
 
 alias define "sdcv"
