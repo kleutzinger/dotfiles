a50281fc878f57e933651e3e56876186fd6cd62b
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-04-12 15:52:19 -0400

    simplify

--- .config/nvim/lua/kevin/plugins/lazy-lsp.lua
+++ .config/nvim/lua/kevin/plugins/lazy-lsp.lua
@@ -1,22 +1,7 @@
 return {
   "dundalek/lazy-lsp.nvim",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/nvim-cmp",
-  },
+  dependencies = { "neovim/nvim-lspconfig" },
   config = function()
-    local lsp_zero = require("lsp-zero")
-
-    lsp_zero.on_attach(function(client, bufnr)
-      -- see :help lsp-zero-keybindings to learn the available actions
-      lsp_zero.default_keymaps({
-        buffer = bufnr,
-        preserve_mappings = false,
-      })
-    end)
-
-    require("lazy-lsp").setup({})
-  end,
+    require("lazy-lsp").setup {}
+  end
 }
