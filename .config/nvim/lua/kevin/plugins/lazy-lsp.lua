f553ded4dc6fea3c184fb23225765565a24f1d72
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-05-05 15:49:31 -0400

    disable pylyzer in lsp

--- .config/nvim/lua/kevin/plugins/lazy-lsp.lua
+++ .config/nvim/lua/kevin/plugins/lazy-lsp.lua
@@ -2,6 +2,10 @@ return {
   "dundalek/lazy-lsp.nvim",
   dependencies = { "neovim/nvim-lspconfig" },
   config = function()
-    require("lazy-lsp").setup {}
+    require("lazy-lsp").setup {
+      excluded_servers = {
+        "pylyzer", -- sometimes maxes out CPU?
+      }
+    }
   end
 }
