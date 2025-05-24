9721ef590dc591e65f3620d0dd5a8d2d28595b27
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-05-24 02:49:23 -0400

    fix game.sh to be uv

--- scripts/game.sh
+++ scripts/game.sh
@@ -2,7 +2,6 @@
 set -ex
 DIRNAME=find-and-play-classic-videogames
 pushd ~/gits/$DIRNAME
-source $HOME/.virtualenvs/++gits+find-and-play-classic-videogames/bin/activate
-python fuzzy_launch.py "$@"
-deactivate
+uv run fuzzy_launch.py "$@"
+# uv run eel_server.py
 popd
