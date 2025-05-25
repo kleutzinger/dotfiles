6b05ebca1c5275c0d18496ce7c5fef9cc0bd46c2
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-05-24 20:37:24 -0400

    fix this because uv

--- .config/fish/functions/eel_server_run.fish
+++ .config/fish/functions/eel_server_run.fish
@@ -1,13 +1,5 @@
-# function eel_server_run
-#     pushd ~/gits/find-classic-games.git
-#     pipenv run python eel_server.py
-#     popd
-# end
-
 function eel_server_run
-    pushd ~/gits/find-classic-games.git
-    source .venv/bin/activate.fish
-    python eel_server.py
-    deactivate
+    pushd ~/gits/find-and-play-classic-videogames
+    uv run eel_server.py
     popd
 end
