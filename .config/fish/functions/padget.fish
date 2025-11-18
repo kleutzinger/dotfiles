54db33ccc8e777cebe6608322ca008bd92ed4366
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-11-18 14:41:25 -0500

    padget and padpost on mac too

--- .config/fish/functions/padget.fish
+++ .config/fish/functions/padget.fish
@@ -1,8 +1,17 @@
 function padget --description 'get message from pad.kevbot.xyz service and copy to clipboard'
     # get message
     set message (curl --silent https://pad.kevbot.xyz/api | jq -r '.message')
-    # save to clipboard
-    echo $message | xsel -ib
+    # save to clipboard (cross-platform)
+    if test (uname) = Darwin
+        echo -n $message | pbcopy
+    else
+        echo $message | xsel -ib
+    end
     set short_message (echo "$message" | head -c 20)
-    notify-send padget "copied to clipboard: $short_message"
+    # notify (cross-platform)
+    if test (uname) = Darwin
+        osascript -e "display notification \"copied to clipboard: $short_message\" with title \"padget\""
+    else
+        notify-send padget "copied to clipboard: $short_message"
+    end
 end
--- .config/fish/functions/padpost.fish
+++ .config/fish/functions/padpost.fish
@@ -1,4 +1,9 @@
 function padpost --description 'post clipboard to pad. run padget on other end'
-  set CLIP (xsel -ob | json_escape)
-  curl -H "Content-Type: application/json" -d "{\"message\": $CLIP}" https://pad.kevbot.xyz/api
+    # get clipboard (cross-platform)
+    if test (uname) = Darwin
+        set CLIP (pbpaste | json_escape)
+    else
+        set CLIP (xsel -ob | json_escape)
+    end
+    curl -H "Content-Type: application/json" -d "{\"message\": $CLIP}" https://pad.kevbot.xyz/api
 end
