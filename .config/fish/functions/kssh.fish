ec8e1d52d603822e935ac993fdf2eedbce616da8
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2026-02-12 12:54:22 -0800

    feat: add kssh function for fuzzy Kubernetes pod selection and exec

---
+++ .config/fish/functions/kssh.fish
@@ -0,0 +1,35 @@
+function kssh --description 'Select a Kubernetes pod with fzf (exec, logs, or describe)'
+    set ns os-analytics-api-dev
+    set mode exec
+
+    # Parse arguments
+    for arg in $argv
+        switch $arg
+            case --logs
+                set mode logs
+            case --describe
+                set mode describe
+            case '*'
+                # Treat anything else as namespace
+                set ns $arg
+        end
+    end
+
+    # Select pod
+    set pod (kubectl -n $ns get pods --no-headers \
+        | fzf \
+        | awk '{print $1}')
+
+    if test -z "$pod"
+        return
+    end
+
+    switch $mode
+        case exec
+            kubectl exec -it $pod -n $ns -- bash
+        case logs
+            kubectl logs -f $pod -n $ns
+        case describe
+            kubectl describe pod $pod -n $ns
+    end
+end
