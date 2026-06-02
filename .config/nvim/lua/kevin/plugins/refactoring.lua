afec37fddf380b62db5226d7ec74f480f0325502
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2026-06-02 09:55:45 -0400

    slightly more working refactor.nvim

--- .config/nvim/lua/kevin/plugins/refactoring.lua
+++ .config/nvim/lua/kevin/plugins/refactoring.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "lewis6991/async.nvim",
   },
   config = function()
     require("refactoring").setup()
