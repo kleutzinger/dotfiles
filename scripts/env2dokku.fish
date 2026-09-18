25396bf8ffadc22a4663a2abb235e469ab889e0a
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2026-09-18 12:02:21 -0400

    env2dokku.fish --interactive

--- scripts/env2dokku.fish
+++ scripts/env2dokku.fish
@@ -1,16 +1,26 @@
 #!/usr/bin/env fish
 
 # this script will read a .env file and output a dokku config:set
-# command
+# command. Pass --interactive to pick which keys to include via fzf
+# (tab to multiselect, enter to confirm).
 
 set env_file ".env"
 set app_name_probably (git rev-parse --show-toplevel | xargs basename)
+
+# all relevant lines in the .env file
+set lines (cat $env_file | grep -v '^#' | grep -v '^$')
+
+if contains -- --interactive $argv
+    set lines (printf '%s\n' $lines | fzf -m --header 'TAB to select keys, ENTER to confirm')
+    if test (count $lines) -eq 0
+        echo "no keys selected" >&2
+        exit 1
+    end
+end
+
 set output "dokku config:set --no-restart $app_name_probably "
 
-
-# loop through all relevant lines in the .env file
-for line in (cat $env_file | grep -v '^#' | grep -v '^$')
-    # encode all values (not keys) with base64
+for line in $lines
     set -l line2 (string split -m 1 = $line)
     set -l key $line2[1]
     set -l value (echo $line2[2] | string trim --chars '\n')
